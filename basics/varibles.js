59d9c2adf85f78021c869948cf1cb35ccf43a521
Yash Kasurde <yashkasurde2002@gmail.com>
2024-04-16 21:41:52 +0530

    variables in js

---
+++ basics/varibles.js
@@ -0,0 +1,8 @@
+const id=12 //once declare in const value can't change again
+let name="yash" // varible declare in let have block scope and can't redeclare in same scope and block scope
+var city="bengaluru"//variable declare in var can be redclared and have globle scope 
+state="maharastra"
+let country;// if value dosen't declare then the output will be undefined
+
+console.log(id)
+console.table([name,city,state.country]) //output will print in table format
